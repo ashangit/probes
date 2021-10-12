a1a1889036cd38c7455c4f7794a41146354d1d6d
n.fraison <n.fraison@criteo.com>
2021-10-12 21:29:22 +0200

    Move from macro to block on code for tokio runtime
    Easier to manage init blocking part like parsing of arguments

--- src/bin/espoke.rs
+++ src/bin/espoke.rs
@@ -1,9 +1,15 @@
 use probes::consul::ConsulClient;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let mut consul_client = ConsulClient::new("kube-worker0005.kubes98.par.preprod.crto.in".to_string(), 8500);
-    consul_client.watch_services().await?;
+fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let mt_rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .thread_name("EsPoke")
+        .build()?;
 
+    mt_rt.block_on(
+        async {
+            let mut consul_client = ConsulClient::new("kube-worker0005.kubes98.par.preprod.crto.in".to_string(), 8500);
+            consul_client.watch_services().await;
+        });
     Ok(())
 }
