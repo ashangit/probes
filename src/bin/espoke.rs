914b02f2e33e4fa5f4949480d637dc4585a68cdd
n.fraison <n.fraison@criteo.com>
2021-10-15 23:49:38 +0200

    Add argparse

--- src/bin/espoke.rs
+++ src/bin/espoke.rs
@@ -1,22 +1,47 @@
 use probes::consul::ConsulClient;
 use console_subscriber;
+use argparse::{ArgumentParser, Store, StoreOption};
+use log::error;
+use std::io::stderr;
 
-fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+fn main() -> Result<(), i32> {
     env_logger::init();
     console_subscriber::init();
 
+    let mut consul_hostanme = "localhost".to_string();
+    let mut consul_port = 8500;
+    let mut services_tag = "".to_string();
+    {  // this block limits scope of borrows by ap.refer() method
+        let mut ap = ArgumentParser::new();
+        ap.set_description("ElasticSearch Probe (Espoke)");
+        ap.refer(&mut consul_hostanme)
+            .add_option(&["--consul-hostname"], Store, "Consul hostname (default: localhost)");
+        ap.refer(&mut consul_port)
+            .add_option(&["--consul-port"], Store, "Consul port (default: 8500)");
+        ap.refer(&mut services_tag)
+            .add_option(&["--services-tag"], Store, "Tag to select services to probe")
+            .required();
+        ap.parse_args_or_exit();
+    }
+
     // TODO: Create dedicated worker pool for probing and use mono thread runtime for services discovery?
     // TODO check all unwrap and improve exception management
-    // TODO command parser
     let mt_rt = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .thread_name("EsPoke")
-        .build()?;
+        .build();
 
-    mt_rt.block_on(
-        async {
-            let mut consul_client = ConsulClient::new("kube-worker0005.kubes98.par.preprod.crto.in".to_string(), 8500, "maintenance-elasticsearch".to_string());
-            consul_client.watch_services().await;
-        });
+    match mt_rt {
+        Err(issue) => {
+            error!("Issue running the multi thread event loop due to: {}", issue);
+            return Err(0)
+        },
+        Ok(mt) => mt.block_on(
+            async {
+                let mut consul_client = ConsulClient::new(consul_hostanme, consul_port, services_tag);
+                consul_client.watch_services().await;
+            }),
+    }
+
     Ok(())
 }
