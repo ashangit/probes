6574b9d58bfbc5b45ce1b1aa1148c970ce379635
n.fraison <n.fraison@criteo.com>
2022-02-19 17:48:13 +0100

    Remove hardcoded absolute path on docker runner

--- test/run.sh
+++ test/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(dirname `realpath "$0"`)"
+
 docker rm dev-consul --force
 docker rm dev-memcached --force
 docker rm dev-grafana --force
 docker rm dev-prometheus --force
-docker run -d --rm --name=dev-consul --network host -v /home/nfraison/dev/perso/rust/probes/test/conf/consul:/consul/config consul
+docker run -d --rm --name=dev-consul --network host -v ${SCRIPT_DIR}/conf/consul:/consul/config consul
 docker run -d --rm --name=dev-memcached --network host memcached -m 128
 docker run -d --rm --name=dev-grafana --network host grafana/grafana:latest
-docker run -d --rm --name=dev-prometheus --network host -v /home/nfraison/dev/perso/rust/probes/test/conf/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus:latest
+docker run -d --rm --name=dev-prometheus --network host -v ${SCRIPT_DIR}/conf/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus:latest
